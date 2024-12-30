bed8b935da1f436669527bd1e6d246bad5f30021
mradul kumar katiyar <karelaking277@gmai.com>
2024-12-30 18:51:17 +0530

    add Contact and Resolutions components to App for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/footer/Footer";
 import PageBackground from "./components/page_background/PageBackground";
 import About from "./pages/about/About";
+import Contact from "./pages/contacts/Contacts";
 import CurrentLearning from "./pages/current_learning/Current_learning";
 import Goals from "./pages/goals/Goals";
 import Hero from "./pages/hero/Hero";
 import Projects from "./pages/projects/Projects";
+import Resolutions from "./pages/resolution/Resolution";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <CurrentLearning />
         <Goals />
         <Projects />
+        <Resolutions/>
+        <Contact/>
         <Footer />
       </div>
     </PageBackground>
