01ac90e75b5bb9350dc6438e16c26ef1e2ef2e20
mradul kumar katiyar <karelaking277@gmai.com>
2024-12-30 17:03:25 +0530

    add Card component for displaying lists with animations and styling

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Card = ({lists}) => {
+    return (
+      <div
+        data-aos="fade-up"
+        className="max-w-4xl mx-auto grid gap-6 md:grid-cols-2"
+      >
+        {lists.map((items, index) => (
+          <div
+            key={index}
+            className="bg-white border-2 shadow-lg rounded-lg p-6 transform transition-all"
+            data-aos="zoom-in"
+          >
+            <h2 className="text-2xl sm:text-lg font-bold text-gray-600 mb-2">
+              {items.title}
+            </h2>
+            <p className="text-gray-400 lg:text-8xl mb-2">{items.description}</p>
+            <a
+              href={items.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 text-sm font-semibold hover:underline"
+            >
+              {items.link ? "Get Code" : ""}
+            </a>
+          </div>
+        ))}
+      </div>
+    );
+}
+
+export default Card
