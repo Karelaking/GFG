8430de099711d51525ed6c3df1aee906c809520c
mradul kumar katiyar <karelaking277@gmai.com>
2024-12-30 18:51:03 +0530

    add Footer component for site-wide footer with author credit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="text-center bg-white  p-4 h-20 flex justify-center items-center">
+      <p className="text-gray-500 text-xl font-bold">
+        Made with ❤️ by{" "}
+        <a
+          href="
+         https://github.com/karelaking"
+          target="_blank"        >
+          Mradul Kumar
+        </a>{" "}
+        &copy; 2024.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
