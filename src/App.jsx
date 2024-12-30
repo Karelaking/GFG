51b5e0cd5316922eb6f6a379c87dbd5ca269167c
mradul kumar katiyar <karelaking277@gmai.com>
2024-12-30 15:54:00 +0530

    refactor App component to integrate new page structure and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from "./components/footer/Footer";
+import PageBackground from "./components/page_background/PageBackground";
+import About from "./pages/about/About";
+import CurrentLearning from "./pages/current_learning/Current_learning";
+import Goals from "./pages/goals/Goals";
+import Hero from "./pages/hero/Hero";
+import Projects from "./pages/projects/Projects";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <PageBackground>
+      <div className="h-full w-screen">
+        <Hero />
+        <About />
+        <CurrentLearning />
+        <Goals />
+        <Projects />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </PageBackground>
+  );
 }
 
-export default App
+export default App;
